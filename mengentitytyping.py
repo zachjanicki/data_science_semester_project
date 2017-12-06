9702dc27c0c35e56ea39bfbc3c41e35c102ca885
michaelmcroskey <michael.mcroskey@gmail.com>
2017-12-05 21:11:32 -0500

    Functionized Meng's code slightly

--- mengentitytyping.py
+++ mengentitytyping.py
@@ -1,5 +1,41 @@
 #!/usr/bin/python
 
+def mine_abbreviations(line, entity_count):
+	arr = line.strip('\r\n').split(' ')
+	n = len(arr)
+	if n < 5: return
+	for i in range(0,n-2):
+		if arr[i] == '(' and arr[i+2] == ')':
+			abbr = arr[i+1]
+			l = len(abbr)
+			if l > 1 and abbr.isalpha():
+				if i >= l and abbr.isupper():
+					isvalid = True
+					for j in range(0,l):
+						if not abbr[l-1-j].lower() == arr[i-1-j][0].lower():
+							isvalid = False
+					if isvalid:
+						phrase = ''
+						for j in range(0,l):
+							phrase = arr[i-1-j]+' '+phrase
+						phrase = phrase[0:-1].lower()
+						if not phrase in entity_count:
+							entity_count[phrase] = 0
+						entity_count[phrase] += 1
+				if i >= l-1 and abbr[-1] == 's' and arr[i-1][-1] == 's' and abbr[0:-1].isupper():
+					isvalid = True
+					for j in range(1,l):
+						if not abbr[l-1-j].lower() == arr[i-j][0].lower():
+							isvalid = False
+					if isvalid:
+						phrase = ''
+						for j in range(1,l):
+							phrase = arr[i-j]+' '+phrase
+						phrase = phrase[0:-1].lower()
+						if not phrase in entity_count:
+							entity_count[phrase] = 0
+						entity_count[phrase] += 1
+
 def mine_entities():
 	
 	paperid_path = []
@@ -17,47 +53,18 @@ def mine_entities():
 	for [paperid,path] in paperid_path:
 		with open(path,'rb')	 as fr:
 			for line in fr:			# for each line
-				arr = line.strip('\r\n').split(' ')
-				n = len(arr)
-				if n < 5: continue
-				for i in range(0,n-2):
-					if arr[i] == '(' and arr[i+2] == ')':
-						abbr = arr[i+1]
-						l = len(abbr)
-						if l > 1 and abbr.isalpha():
-							if i >= l and abbr.isupper():
-								isvalid = True
-								for j in range(0,l):
-									if not abbr[l-1-j].lower() == arr[i-1-j][0].lower():
-										isvalid = False
-								if isvalid:
-									phrase = ''
-									for j in range(0,l):
-										phrase = arr[i-1-j]+' '+phrase
-									phrase = phrase[0:-1].lower()
-									if not phrase in entity_count:
-										entity_count[phrase] = 0
-									entity_count[phrase] += 1
-							if i >= l-1 and abbr[-1] == 's' and arr[i-1][-1] == 's' and abbr[0:-1].isupper():
-								isvalid = True
-								for j in range(1,l):
-									if not abbr[l-1-j].lower() == arr[i-j][0].lower():
-										isvalid = False
-								if isvalid:
-									phrase = ''
-									for j in range(1,l):
-										phrase = arr[i-j]+' '+phrase
-									phrase = phrase[0:-1].lower()
-									if not phrase in entity_count:
-										entity_count[phrase] = 0
-									entity_count[phrase] += 1
+#				print "===" + line + "==="
+				mine_abbreviations(line, entity_count)
+#				break
 
-		
-	with open('data/entity_count.txt','w') as fw:
-		col_width = (max(len(phrase) for phrase in entity_count))
-		for [phrase,count] in sorted(entity_count.items(), key=lambda x:-x[1]):
-			spaces = col_width - len(phrase) + 3
-			fw.write(phrase + ' '*spaces + str(count) + '\n')
+#	mine_abbreviations("the Support Vector Machines ( SVMs ) are not very good Unilateral Decision ( UD ) making.", entity_count)			
+	print entity_count
+
+#	with open('data/entity_count.txt','w') as fw:
+#		col_width = (max(len(phrase) for phrase in entity_count))
+#		for [phrase,count] in sorted(entity_count.items(), key=lambda x:-x[1]):
+#			spaces = col_width - len(phrase) + 3
+#			fw.write(phrase + ' '*spaces + str(count) + '\n')
 
 def type_entities():
 	n_type = 4
@@ -171,6 +178,5 @@ def type_entities():
 		fw.write(s + '\n')
 	fw.close()
 
-#mine_entities()
-type_entities()
-
+mine_entities()
+#type_entities()
