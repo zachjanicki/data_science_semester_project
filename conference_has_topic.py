f7d75ad235403414d05d5b7c95a289ddbf52e2cf
michaelmcroskey <michael.mcroskey@gmail.com>
2017-12-06 14:25:50 -0500

    Ignore virtual environment files

---
+++ conference_has_topic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from sklearn.naive_bayes import GaussianNB
+
+def conference_has_topic(db, problem, method, conference):
+	
+	return True
+#	return False
+	
+if __name__ == "__main__":
+	print "Test"
+#	conn = sqlite3.connect('data/database.db')
+#	db = conn.cursor()
+#
+#	command = '''SELECT * FROM Entities'''
+#	db.execute(command)
+#	result = db.fetchall()
+#	
+#	types = ['METHOD','PROBLEM','DATASET','METRIC']
+#	
+#	print conference_has_topic(db, problem, method, conference)
