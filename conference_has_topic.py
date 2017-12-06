ba7f2fb8328055aa5633fd01fabc4818dd7fa6c9
michaelmcroskey <michael.mcroskey@gmail.com>
2017-12-06 15:49:32 -0500

    Significantly cleaned up conference_has_topic.py

--- conference_has_topic.py
+++ conference_has_topic.py
@@ -1,46 +1,45 @@
 #!/usr/bin/python
 
+import numpy as np
 from sklearn.datasets import fetch_20newsgroups
-conferences = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
-twenty_train = fetch_20newsgroups(subset='train', categories=conferences, shuffle=True, random_state=42)
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
-docs_new = ['God is love', 'OpenGL on the GPU is fast']
-
 from sklearn.pipeline import Pipeline
+
+# Global vars ----------------------------
+conferences = ['idcm', 'kdd','wsdm', 'www']
+
+# Train Data -----------------------------
+training = ['information latent dirichlet information allocation information', 'support vector machine', 'expectation maximization', 'world wide web', 'singular value decomposition']
+targets = [0, 1, 2, 3, 2]
 text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
-text_clf.fit(twenty_train.data, twenty_train.target)
+text_clf.fit(training, targets)
 
-import numpy as np
-twenty_test = fetch_20newsgroups(subset='test',categories=conferences, shuffle=True, random_state=42)
-docs_test = twenty_test.data
-print len(twenty_test.data)
-predicted = text_clf.predict(docs_test)
+# Test Data ------------------------------
+testing = ['information retrieval', 'neural information processing systems']
+testing_targets = [0, 0]
+predicted = text_clf.predict(testing)
 
-for doc, conference in zip(docs_test, predicted):
-	print('%r => %s' % (doc[0:10], twenty_train.target_names[conference]))
-
-#print len(docs_test)
-#print predicted
-#print twenty_test.target
-#print np.mean(predicted == twenty_test.target)
-
-def conference_has_topic(db, problem, method, conference):
+for doc, conference in zip(testing, predicted):
+	print('%r => %s' % (doc, conferences[conference]))
 	
-	return True
-#	return False
-	
-if __name__ == "__main__":
-	print "Test"
-	conn = sqlite3.connect('data/database.db')
-	db = conn.cursor()
+print np.mean(predicted == testing_targets)
 
-	command = '''SELECT * FROM Entities'''
-	db.execute(command)
-	result = db.fetchall()
-	
-	types = ['METHOD','PROBLEM','DATASET','METRIC']
-	
-	print conference_has_topic(db, problem, method, conference)
+#def conference_has_topic(db, problem, method, conference):
+#	
+#	return True
+##	return False
+#	
+#if __name__ == "__main__":
+#	print "Test"
+#	conn = sqlite3.connect('data/database.db')
+#	db = conn.cursor()
+#
+#	command = '''SELECT * FROM Entities'''
+#	db.execute(command)
+#	result = db.fetchall()
+#	
+#	types = ['METHOD','PROBLEM','DATASET','METRIC']
+#	
+#	print conference_has_topic(db, problem, method, conference)
